1fb3ec8efabad3d69fb262ebad1bf36c9b1826fd
Dasar <ibmpc.maniac@gmail.com>
2015-05-04 17:51:35 +0500

    Light theme for sample app

--- sample/src/uz/shift/colorpicker/MainActivity.java
+++ sample/src/uz/shift/colorpicker/MainActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	protected static final String TAG = "ShiftPicker";
 
 	private LineColorPicker colorPicker;
+
+	private TextView tvColor;
 
 	String[] pallete = new String[] { "#b8c847", "#67bb43", "#41b691",
 			"#4182b6", "#4149b6", "#7641b6", "#b741a7", "#c54657", "#d1694a" };
@@ -20,6 +23,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		colorPicker = (LineColorPicker) findViewById(R.id.picker);
+		tvColor = (TextView) findViewById(R.id.textViewColor);
 
 		int[] colors = new int[pallete.length];
 
@@ -33,13 +37,28 @@ public class MainActivity extends Activity {
 		// Set selected color
 		colorPicker.setColor(colors[2]);
 
+		// Get selected color
+		int color = colorPicker.getColor();
+
+		updateColor(color);
+
 		colorPicker.setOnColorChangedListener(new OnColorChangedListener() {
 
 			@Override
 			public void onColorChanged(int c) {
 				Log.d(TAG, "Selectede color " + Integer.toHexString(c));
+
+				updateColor(c);
 			}
 		});
 
+	}
+
+	private void updateColor(int color) {
+		String hex = Integer.toHexString(color);
+
+		hex = hex.toUpperCase();
+
+		tvColor.setText(String.format("Selected color: #%s", hex));
 	}
 }
